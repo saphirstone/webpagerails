68b2db765775c51840ec4f33c01d664f885b2734
sax <hasiniainasaphir@gmail.com>
2018-07-23 15:00:37 +0300

    first page rails

---
+++ app.rb
@@ -0,0 +1,12 @@
+class GossipProjectApp < Sinatra::Base
+  get '/' do
+    "<html>
+      <head><title>The Gossip Project</title></head>
+      <body>
+        <h1>Mon super site de gossip !hey ohhhhhh</h1>
+      </body>
+    </html>"
+  end
+
+  
+end
